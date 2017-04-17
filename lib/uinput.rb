53a6fddbb56f4d10c6a8c7b5a6584372222be8fd
Christopher Aue <mail@christopheraue.net>
2017-04-18 00:34:23 +0200

    added Uinput.open_file and .version

--- lib/uinput.rb
+++ lib/uinput.rb
@@ -1,6 +1,30 @@
 require 'bundler/setup'
 require 'linux_input'
+require 'fcntl'
 require "uinput/version"
 require 'uinput/uinput.rb'
 
 Uinput = LinuxInput::Uinput
+
+module Uinput
+  FILE_PATH = '/dev/uinput'
+
+  def self.open_file(mode = Fcntl::O_RDWR)
+    File.open(FILE_PATH, mode)
+  end
+
+  def self.version
+    @version ||= begin
+      pack_size = case FFI.type_size(:uint)
+        when 2 then 's'
+        when 4 then 'L'
+        when 8 then 'Q'
+      end
+
+      version = [0].pack pack_size
+      open_file.ioctl UI_GET_VERSION, version
+      version.unpack(pack_size).first
+    end
+  end
+end
+
